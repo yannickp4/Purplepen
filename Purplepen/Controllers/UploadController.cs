6817a3d519760df84d37d4feacdf9272f9ba209f
yannickp4 <yannickpaternoster@gmail.com>
2013-12-03 11:39:58 +0100

    portfolio weergave

--- Purplepen/Controllers/UploadController.cs
+++ Purplepen/Controllers/UploadController.cs
@@ -12,7 +12,7 @@ namespace Purplepen.Controllers
     {
         //
         // GET: /Upload/
- public ActionResult Upload()
+        public ActionResult Upload()
         {
             Upload u = new Upload();
             ViewBag.allProjects = u.allProjects(65465416);//session id
@@ -26,7 +26,7 @@ namespace Purplepen.Controllers
 
         // This action handles the form POST and the upload
         [HttpPost]
-        public ActionResult ImageUpload(HttpPostedFileBase file,FormCollection form)
+        public ActionResult ImageUpload(HttpPostedFileBase file, FormCollection form)
         {
 
             // Verify that the user selected a file
@@ -36,42 +36,45 @@ namespace Purplepen.Controllers
                 var fileName = Path.GetFileName(file.FileName);
                 // extract file extension
                 var fileExtension = Path.GetExtension(fileName);
-                if(fileExtension==".jpg" || fileExtension==".JPG" || fileExtension=="jpeg" || fileExtension=="JPEG" || fileExtension==".gif" || fileExtension==".png"){
+                if (fileExtension == ".jpg" || fileExtension == ".JPG" || fileExtension == "jpeg" || fileExtension == "JPEG" || fileExtension == ".gif" || fileExtension == ".png")
+                {
                     //get datetime
-                    String now=DateTime.Now.ToString("yyyy");
+                    String now = DateTime.Now.ToString("yyyy");
                     now += DateTime.Now.ToString("MM");
                     now += DateTime.Now.ToString("dd");
                     now += DateTime.Now.ToString("HH");
                     now += DateTime.Now.ToString("mm");
                     now += DateTime.Now.ToString("ss");
                     // store the file inside ~/App_Data/uploads folder
-                    var path = Path.Combine(Server.MapPath("~/uploads/"), now+fileName);
+                    var path = Path.Combine(Server.MapPath("~/uploads/"), now + fileName);
                     file.SaveAs(path);
                     //save into database
                     project p = new project();
                     uploadversion u = new uploadversion();
-                    p.user_id=65465416; //Session["userId"]
+                    p.user_id = 65465416; //Session["userId"]
                     var blap = form["project_id"];
                     if (form["project_id"] != null)
                     {
                         p.project_id = Convert.ToInt32(form["project_id"]);
                         u.project_id = Convert.ToInt32(form["project_id"]);
                     }
-                    else {
+                    else
+                    {
+                        //check if title already exist
                         p.name = form["txtTitle"];
                         u.version_id = 0;
                     }
-                    u.path = now+fileName;
-                    u.description=form["txtDescription"];
-                    u.datum=DateTime.Now;
+                    u.path = now + fileName;
+                    u.description = form["txtDescription"];
+                    u.datum = DateTime.Now;
                     Upload uploadimage = new Upload();
-                    uploadimage.uploadimage(u,p);
+                    uploadimage.uploadimage(u, p);
                     // redirect back to the index action to show the form once again
                     return RedirectToAction("Upload");
                 }
-                
+
             }
-return RedirectToAction("Error");
+            return RedirectToAction("Error");
         }
 
     }
--- Purplepen/Models/Upload.cs
+++ Purplepen/Models/Upload.cs
@@ -9,13 +9,16 @@ namespace Purplepen.Models
     {
         dcPurplepenDataContext dc = new dcPurplepenDataContext();
 
-        public class ownproject {
-            public String latestVersion{ get; set; }
+        public class ownproject
+        {
+            public String latestVersion { get; set; }
             public String title { get; set; }
             public int projectId { get; set; }
+            public DateTime datum { get; set; }
         }
 
-        public void uploadimage(uploadversion image,project project) {
+        public void uploadimage(uploadversion image, project project)
+        {
             if (project.project_id == 0)
             {
                 dc.projects.InsertOnSubmit(project);
@@ -23,20 +26,36 @@ namespace Purplepen.Models
                 var projectid = (from p in dc.projects where p.user_id == project.user_id && p.name == project.name select p.project_id).Single();
                 image.project_id = Convert.ToInt32(projectid);
             }
-            
+
             dc.uploadversions.InsertOnSubmit(image);
             dc.SubmitChanges();
         }
 
-        public List<project> allProjects(int user_id) {
+        public List<project> allProjects(int user_id)
+        {
             var result = (from p in dc.projects where p.user_id == user_id select p).ToList();
             return result;
         }
 
         public List<ownproject> allOwnProjects(int user_id)
         {
-            var result = (from p in dc.projects join i in dc.uploadversions on p.project_id equals i.project_id select new ownproject { title=p.name,projectId=p.project_id,latestVersion=i.path}).Distinct().ToList();
-            return result;
+            List<ownproject> realresult = new List<ownproject>();
+            int blap = 0;
+            var result = (from p in dc.projects join i in dc.uploadversions on p.project_id equals i.project_id select new ownproject { title = p.name, projectId = p.project_id, latestVersion = i.path, datum = i.datum }).ToList();
+            for (int x = 0; x < result.Count(); x++)
+            {
+                realresult.Add(result[x]);
+                for (int y = 0; y < result.Count(); y++)
+                {
+                    if (realresult[blap].projectId == result[y].projectId && realresult[blap].datum < result[y].datum)
+                    {
+                        realresult.Remove(realresult.Last());
+                        blap--;
+                    }
+                }
+                blap++;
+            }
+            return realresult;
         }
 
     }

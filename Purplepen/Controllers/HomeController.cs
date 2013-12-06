3ba091498c563a2818f27a5278a5974b666b4594
unknown <Samvanhoof@hotmail.com>
2013-12-06 13:30:57 +0100

    comment en dots

--- Purplepen/Controllers/HomeController.cs
+++ Purplepen/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Purplepen.Models;
 
 namespace Purplepen.Controllers
 {
@@ -11,10 +12,49 @@ namespace Purplepen.Controllers
         //
         // GET: /Home/
 
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            Comment comm = new Comment();
+            comment c = new comment();
+            dot d = new dot();
+
+            c.description = form["commentText"];
+            c.upload_id = 1; //aanpassen
+            c.user_id = 2; //aanpassen
+            c.title = "general"; //aanpassen
+            c.score = 0; //aanpassen
+            c.datum = DateTime.Now;
+            c.category = 0;
+            c.dot_id = comm.getID() + 1; 
+          
+            d.dot_x = Convert.ToInt32(form["dotX"]);
+            d.dot_y = Convert.ToInt32(form["dotY"]);
+
+            if (string.IsNullOrEmpty(form["dotY"]))
+            {
+                comm.placeComment(c);
+            }
+            else
+            {
+                comm.placeComment(c);
+                comm.placeDot(d);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
         public ActionResult Index()
         {
+            Comment comm = new Comment();
+            ViewBag.allcomments = comm.allComments();
+            ViewBag.alldots = comm.allDots(); 
             return View();
+
         }
+
 
     }
 }
--- Purplepen/Models/Comment.cs
+++ Purplepen/Models/Comment.cs
@@ -6,15 +6,87 @@ using System.Web.Mvc;
 
 namespace Purplepen.Models
 {
-    public class Comment : Controller
+    public class Comment
     {
-        //
-        // GET: /Comment/
 
-        public ActionResult Index()
+        private dcPurplepenDataContext dc = new dcPurplepenDataContext();
+
+        public void placeComment(comment c)
         {
-            return View();
+            dc.comments.InsertOnSubmit(c);
+            dc.SubmitChanges();
         }
+
+        public void placeDot(dot d)
+        {
+            dc.dots.InsertOnSubmit(d);
+            dc.SubmitChanges();
+        }
+
+        public int getID()
+        {
+            var id = (from d in dc.dots select d).OrderByDescending(d => d.dot_id).FirstOrDefault();
+            var idL = id.dot_id;
+            return idL;
+        }
+
+        public class commentName
+        {
+            public String description { get; set; }
+            public String naam { get; set; }
+        }
+
+        public List<commentName> allComments()
+        {
+            // var result = (from c in dc.comments select c).ToList();
+            var result = (from c in dc.comments
+                          join u in dc.users on c.user_id equals u.user_id
+                          select new commentName
+                          {
+                              naam = u.name,
+                              description = c.description
+                          }).ToList();
+            return result;
+        }
+
+        public class dotXY
+        {
+            public int ID { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public String description { get; set; }
+            public String naam { get; set; }
+        }
+
+        public List<dotXY> allDots()
+        {
+            var result = (from d in dc.dots
+                          join c in dc.comments on d.dot_id equals c.dot_id
+                          join u in dc.users on c.user_id equals u.user_id
+                          select new dotXY
+                          {
+                              ID = d.dot_id,
+                              X = d.dot_x,
+                              Y = d.dot_y,
+                              description = c.description,
+                              naam = u.name
+                          }).ToList();
+            return result;
+        }
+
+        //public List<dot>allDots()
+        //{
+        //    // var result = (from c in dc.comments select c).ToList();
+        //    var result = (from d in dc.dots select last d).ToList();
+                          
+                         
+        //    return result;
+        //}
+
+
+
+
+
 
     }
 }
